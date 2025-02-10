3cf7e5ebe4af99183ff752a7fdc7a741a83a8212
MuaazShoaib123 <muaazrt@gmail.com>
2025-02-10 16:37:44 +0500

    Lecture Page Changed

--- client/src/components/lecture.jsx
+++ client/src/components/lecture.jsx
@@ -2,21 +2,38 @@ import { Canvas } from "@react-three/fiber";
 import { Experience } from "./Experience";
 import "../styles/lecture.css";
 import React from "react";
-import Sidebar from "./sidebar";
 import { useParams } from "react-router-dom";
 
 function Lecture() {
+  const { courseId } = useParams();
 
-  const { courseId} = useParams(); 
   return (
     <div className="lecturers-page">
-      <Sidebar />
+      {/* Heading */}
+      <h2 className="course-name">Selected CourseId: {courseId}</h2>
+
+      {/* Main Content */}
       <div className="content">
-        <h2 className="course-name"> Selected CourseId :{courseId}</h2>
-        <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
-          <color attach="background" args={["#ececec"]} />
-          <Experience />
-        </Canvas>
+        {/* 3D Avatar Section */}
+        <div className="avatar-section">
+          <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
+            <color attach="background" args={["#ececec"]} />
+            <Experience />
+          </Canvas>
+        </div>
+
+        {/* Video Placeholder Section */}
+        <div className="video-placeholder">
+          <p>Video Placeholder</p>
+        </div>
+
+        {/* Chatbot Section */}
+        <div className="chatbot-container">
+          <div className="chatbot-box">
+            <input type="text" placeholder="Type a message..." />
+            <button>Send</button>
+          </div>
+        </div>
       </div>
     </div>
   );
